8724f6cba9cb79713d30b632b3db8305b8a92ff4
ExistentialCrisis2025 <212199316+ExistentialCrisis2025@users.noreply.github.com>
2025-07-25 23:53:18 +0530

    Provided the solution for 06-Repeat Strings

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(txt,num) {
+    let ans = "";
 
+    if(num<0){
+        return "ERROR";
+    }
+    for(let i=0;i<num;i++){
+        ans  = ans + txt;
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
