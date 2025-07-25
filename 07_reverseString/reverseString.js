19274fd108a10d88e612d29d38394cdef190c7ce
ExistentialCrisis2025 <212199316+ExistentialCrisis2025@users.noreply.github.com>
2025-07-26 00:21:01 +0530

    Provided the solution for 07-Reverse String

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(txt) {
+    let temp = txt.split('');
+    let i = 0;
+    let j = temp.length - 1;
+    while(i<=j){
+        let t = temp[i];
+        temp[i] = temp[j];
+        temp[j] = t;
+        i++;
+        j--;
+    }
+    return temp.join('');
 };
 
 // Do not edit below this line
