a1996cbb8b0b452e8241b28aa401b7226b65f80b
ExistentialCrisis2025 <212199316+ExistentialCrisis2025@users.noreply.github.com>
2025-07-29 21:23:49 +0530

    Provided the solution for 11-tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  n -= 32;
+  n *= 5;
+  n /= 9;
+  return roundNumber(n,1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(n) {
+  n*=9;
+  n/=5;
+  n+=32;
+  return roundNumber(n,1);
 };
+
+function roundNumber(n,dec){
+  return Math.round(n * Math.pow(10,dec)) / Math.pow(10,dec);
+}
 
 // Do not edit below this line
 module.exports = {
