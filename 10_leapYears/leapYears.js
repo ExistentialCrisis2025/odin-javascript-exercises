0ca338e73dc166f0a161bda5dc201ea1a75551ca
ExistentialCrisis2025 <212199316+ExistentialCrisis2025@users.noreply.github.com>
2025-07-29 21:15:47 +0530

    Provided the solution for 10-leapYear

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(n) {
+    if(n%4==0){
+        if(n%100==0){
+            if(n%400==0){
+                return true;
+            }
+        } else{
+            return true;
+        }
+    }
 
+    return false;
 };
 
 // Do not edit below this line
