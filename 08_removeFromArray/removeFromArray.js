769d858ab165797367af24d1d1934ce3e4409c2f
ExistentialCrisis2025 <212199316+ExistentialCrisis2025@users.noreply.github.com>
2025-09-01 12:18:59 +0530

    Solved q8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,n) {
+    for(let i=1;i<=arguments.length;i++){
+        arr = arr.filter((a) => a!==n);
+    }
+    return arr;
 };
 
 // Do not edit below this line
